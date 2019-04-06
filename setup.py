891f5281257f53fbf565749a4c2f02f237297f0c
idle sign <idlesign@yandex.ru>
2019-04-06 10:42:44 +0700

    Added QA for py3.7

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: BSD License'
     ],
 )
