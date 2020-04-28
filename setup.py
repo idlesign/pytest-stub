e5881a844f03236aa677a5b55d093f7cf9aefd60
idle sign <idlesign@yandex.ru>
2020-04-28 16:51:20 +0700

    Classifier updated.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
 
     classifiers=[
         # As in https://pypi.python.org/pypi?:action=list_classifiers
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Framework :: Pytest',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Testing',
